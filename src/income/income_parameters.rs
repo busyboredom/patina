278b4cf148790f49121f4f3143c0a417a6e2ca6c
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2020-08-06 18:43:47 -0400

    Add Income Parameters

---
+++ src/income/income_parameters.rs
@@ -0,0 +1,15 @@
+pub struct IncomeParameters {
+
+}
+
+impl IncomeParameters {
+    pub fn new() -> IncomeParameters {
+        IncomeParameters {}
+    }
+}
+
+impl Default for IncomeParameters {
+    fn default() -> Self {
+        IncomeParameters::new()
+    }
+}
