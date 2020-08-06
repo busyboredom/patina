f5a9cd79ad26e27c678cf23c8d6999f0ff4a0880
Charlie Wilkin <cwilkin@protonmail.com>
2020-08-06 18:06:28 -0400

    Add food parameter struct

---
+++ src/food/mod.rs
@@ -0,0 +1,3 @@
+pub mod food;
+
+pub use food::FoodParameters;
--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,16 @@ pub mod transport;
 pub struct Budget {
     pub income: f64,
     pub age: i32,
+    pub food_params: food::FoodParameters,
 }
 
 impl Budget {
     pub fn new (income: f64, age: i32) -> Budget {
+        let food_params = food::FoodParameters {};
         Budget {
             income,
             age,
+            food_params,
         }
     }
 }
