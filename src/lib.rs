17b7f7e9282fef9d4d936125fefe3423b48718d4
Charlie Wilkin <cwilkin@protonmail.com>
2020-08-06 17:51:42 -0400

    Add modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+pub mod food;
+pub mod healthcare;
+pub mod housing;
+pub mod income;
+pub mod leisure;
+pub mod misc;
+pub mod saving;
+pub mod taxes;
+pub mod transport;
+
 pub struct Budget {
     pub income: f64,
     pub age: i32,
