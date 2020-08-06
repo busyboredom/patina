2395226cfaa50e1dd4299d090a647ada0385b4c6
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2020-08-06 18:37:57 -0400

    Add leisure parameters

---
+++ src/leisure/leisure_parameters.rs
@@ -0,0 +1,15 @@
+pub struct LeisureParameters {
+
+}
+
+impl LeisureParameters {
+    pub fn new() -> LeisureParameters {
+        LeisureParameters {}
+    }
+}
+
+impl Default for LeisureParameters {
+    fn default() -> Self {
+        LeisureParameters::new()
+    }
+}
