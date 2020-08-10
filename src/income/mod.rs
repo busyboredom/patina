a3f66ac023e4b80800410a453097e8c65c9d42a1
Charlie WIlkin <cwilkin@protonmail.com>
2020-08-10 07:36:11 -0400

    Bring Job struct and Pay enum into Income scope

--- src/income/mod.rs
+++ src/income/mod.rs
@@ -2,4 +2,4 @@ pub mod income_calculations;
 pub mod income_parameters;
 
 pub use income_calculations::*;
-pub use income_parameters::IncomeParameters;
+pub use income_parameters::{IncomeParameters, Job, Pay};
